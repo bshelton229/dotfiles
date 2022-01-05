94fd7b91cd48ac323e8c73505d49f6e5afde5f01
Bryan Shelton <bryan@rover.com>
2022-01-05 15:08:39 -0800

    Symlink all .dotfiles

--- install.sh
+++ install.sh
@@ -4,4 +4,11 @@ set -euo pipefail
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
-ln -s ${SCRIPT_DIR}/.gitconfig ~/.gitconfig
+# ln -s ${SCRIPT_DIR}/.gitconfig ~/.gitconfig
+
+for filename in ${SCRIPT_DIR}/.*; do
+    basename=$(basename $filename)
+    if [[ -f $filename ]]; then
+        ln -s $filename ~/$basename
+    fi
+done
