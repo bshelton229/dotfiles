e61757338225b2b12aaf29045651791ab865e34e
Bryan Shelton <bryan@rover.com>
2022-01-06 08:36:57 -0800

    Put the hook in place

--- install.sh
+++ install.sh
@@ -8,9 +8,15 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 # ln -s ${SCRIPT_DIR}/.gitconfig ~/.gitconfig
 # ln -s ${SCRIPT_DIR}/.gitignore-global ~/.gitignore-global
 
+# Symlink all root level dotfiles to the home folder
 for filename in ${SCRIPT_DIR}/.*; do
     basename=$(basename $filename)
-    if [[ "${basename}" != ".." ]] && [[ "${basename}" != "." ]] && [[ "${basename}" != ".git" ]]; then
+    if [[ -f $filename ]]; then
         ln -s $filename ~/$basename
     fi
+done
+
+for hook in ${SCRIPT_DIR}/git-hooks/*; do
+    basename=$(basename $hook)
+    ln -s $hook $CODESPACE_VSCODE_FOLDER/.git/$basename
 done
