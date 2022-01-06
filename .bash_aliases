a45827b874eb1774c1c67c3e24317b7a8f599755
Bryan Shelton <bryan@rover.com>
2022-01-06 10:18:06 -0800

    Helper to install git hooks

--- .bash_aliases
+++ .bash_aliases
@@ -2,8 +2,9 @@ function gitbr() {
     git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/\1/'
 }
 
+# Trying to put this in install.sh had a race condition where the workspace wasn't yet cloned.
 function link_git_hooks() {
-    for hook in ${SCRIPT_DIR}/git-hooks/*; do
+    for hook in /workspaces/.codespaces/.persistedshare/dotfiles/git-hooks/*; do
         basename=$(basename $hook)
         if [[ -d $CODESPACE_VSCODE_FOLDER/.git ]]; then
             ln -s $hook $CODESPACE_VSCODE_FOLDER/.git/hooks/$basename
--- install.sh
+++ install.sh
@@ -4,10 +4,6 @@ set -euo pipefail
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
-# ln -s ${SCRIPT_DIR}/.bash_aliases ~/.bash_aliases
-# ln -s ${SCRIPT_DIR}/.gitconfig ~/.gitconfig
-# ln -s ${SCRIPT_DIR}/.gitignore-global ~/.gitignore-global
-
 # Symlink all root level dotfiles to the home folder
 for filename in ${SCRIPT_DIR}/.*; do
     basename=$(basename $filename)
@@ -15,11 +11,3 @@ for filename in ${SCRIPT_DIR}/.*; do
         ln -s $filename ~/$basename
     fi
 done
-
-# Wait for clone
-# for hook in ${SCRIPT_DIR}/git-hooks/*; do
-#     basename=$(basename $hook)
-#     if [[ -d $CODESPACE_VSCODE_FOLDER/.git ]]; then
-#         ln -s $hook $CODESPACE_VSCODE_FOLDER/.git/hooks/$basename
-#     fi
-# done
