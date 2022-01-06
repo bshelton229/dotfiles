048892ae3472e8d451165757dec05ffdcd0129c9
Bryan Shelton <bryan@rover.com>
2022-01-06 08:31:10 -0800

    Try the script again

--- install.sh
+++ install.sh
@@ -4,13 +4,13 @@ set -euo pipefail
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
-ln -s ${SCRIPT_DIR}/.bash_aliases ~/.bash_aliases
-ln -s ${SCRIPT_DIR}/.gitconfig ~/.gitconfig
-ln -s ${SCRIPT_DIR}/.gitignore-global ~/.gitignore-global
+# ln -s ${SCRIPT_DIR}/.bash_aliases ~/.bash_aliases
+# ln -s ${SCRIPT_DIR}/.gitconfig ~/.gitconfig
+# ln -s ${SCRIPT_DIR}/.gitignore-global ~/.gitignore-global
 
-# for filename in ${SCRIPT_DIR}/.*; do
-#     basename=$(basename $filename)
-#     if [[ -f $filename ]]; then
-#         ln -s $filename ~/$basename
-#     fi
-# done
+for filename in ${SCRIPT_DIR}/.*; do
+    basename=$(basename $filename)
+    if [[ "${basename}" != ".." ]] && [[ "${basename}" != "." ]] && [[ "${basename}" != ".git" ]]; then
+        ln -s $filename ~/$basename
+    fi
+done
