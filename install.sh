8da42ed73ed671ec8657b599bb20bcb621bd0deb
Bryan Shelton <bryan@rover.com>
2022-01-06 12:05:56 -0800

    Testing startup

--- install.sh
+++ install.sh
@@ -11,3 +11,17 @@ for filename in ${SCRIPT_DIR}/.*; do
         ln -s $filename ~/$basename
     fi
 done
+
+for hook in /workspaces/.codespaces/.persistedshare/dotfiles/git-hooks/*; do
+    basename=$(basename $hook)
+    echo "Is ${CODESPACE_VSCODE_FOLDER} real" >> /tmp/dotfiles.log
+    ls -l ${CODESPACE_VSCODE_FOLDER} >> /tmp/dotfiles.log
+    sleep 20
+    ls -l ${CODESPACE_VSCODE_FOLDER} >> /tmp/dotfiles.log
+
+    if [[ -d $CODESPACE_VSCODE_FOLDER/.git ]]; then
+        ln -s $hook $CODESPACE_VSCODE_FOLDER/.git/hooks/$basename
+    else
+        echo "Not a directory" >> /tmp/dotfiles.log
+    fi
+done
